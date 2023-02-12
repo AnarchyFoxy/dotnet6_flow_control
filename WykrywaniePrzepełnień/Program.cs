b6c779ddbb71bd299e6f9983d5c8311a9634442c
Astryda Malinowska <astamalinowska@outlook.com>
2023-02-12 20:15:44 +0100

    Finished with overflows

--- WykrywaniePrzepełnień/Program.cs
+++ WykrywaniePrzepełnień/Program.cs
@@ -1,1 +1,79 @@
 ﻿using static System.Console;
+
+int x = int.MaxValue - 1;
+WriteLine($"Wartość początkowa: {x}");
+x++;
+WriteLine($"Po inkrementacji: {x}");
+x++;
+WriteLine($"Po inkrementacji: {x}");
+x++;
+WriteLine($"Po inkrementacji: {x}");
+// Output:
+/*
+Wartość początkowa: 2147483646
+Po inkrementacji: 2147483647
+Po inkrementacji: -2147483648
+Po inkrementacji: -2147483647
+*/
+
+//checked
+//{
+//    int y = int.MaxValue - 1;
+//    WriteLine($"Wartość początkowa: {y}");
+//    y++;
+//    WriteLine($"Po inkrementacji: {y}");
+//    y++;
+//    WriteLine($"Po inkrementacji: {y}");
+//    y++;
+//    WriteLine($"Po inkrementacji: {y}");
+//}
+// Output:
+/*
+Wartość początkowa: 2147483646
+Po inkrementacji: 2147483647
+Unhandled exception. System.OverflowException: Arithmetic operation resulted in an overflow.
+ */
+
+try
+{
+    checked
+    {
+        int y = int.MaxValue - 1;
+        WriteLine($"Wartość początkowa: {y}");
+        y++;
+        WriteLine($"Po inkrementacji: {y}");
+        y++;
+        WriteLine($"Po inkrementacji: {y}");
+        y++;
+        WriteLine($"Po inkrementacji: {y}");
+    }
+}
+catch (OverflowException)
+{
+    WriteLine("No i się przepełniło, ale złapałam");
+}
+// Output:
+/*
+Wartość początkowa: 2147483646
+Po inkrementacji: 2147483647
+No i się przepełniło, ale złapałam
+ */
+
+unchecked
+{
+    int y = int.MaxValue - 1;
+    WriteLine($"Wartość początkowa: {y}");
+    y++;
+    WriteLine($"Po inkrementacji: {y}");
+    y++;
+    WriteLine($"Po inkrementacji: {y}");
+    y++;
+    WriteLine($"Po inkrementacji: {y}");
+}
+// Output
+/*
+Wartość początkowa: 2147483646
+Po inkrementacji: 2147483647
+Po inkrementacji: -2147483648
+Po inkrementacji: -2147483647
+ */
